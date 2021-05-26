f4820d89fe0baa9e5b3d836dd9077bb612cadf6f
Arthur Martinelli <ama18@inf.ufpr.br>
2021-05-26 19:01:37 -0300

    rascunho do p3 feito

--- ppos_core.c
+++ ppos_core.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ppos_data.h"
+#include "queue.h"
 
-#define DEBUG
+//#define DEBUG
 
+//definições de variáveis globais
 int task_count=0;
+int user_tasks=0;
 task_t *task_atual;
-task_t task_main;
+task_t task_main, task_dispatcher;
+task_t *ready_task_queue=NULL;
 
 void ppos_init() {
 
@@ -16,9 +20,13 @@ void ppos_init() {
     getcontext(&task_main.context);
     task_atual = &task_main;
 
+    task_create(&task_dispatcher, dispatcher, NULL);
+
     #ifdef DEBUG
     printf("inicialização completa com sucesso\n");
     #endif
+
+    task_yield();
 
 }
 
@@ -45,6 +53,10 @@ int task_create (task_t *task, void (*start_routine)(void *), void *arg) {
     makecontext(&task->context , (void*)(start_routine), 1, arg);
     task->id = ++task_count;
 
+    if (task != &task_dispatcher) {     //contador de tarefas do usuário
+        ++user_tasks;
+        queue_append((queue_t **) &ready_task_queue, (queue_t*) task);
+
     #ifdef DEBUG
     printf("task_create: tarefa %d criada\n", task->id);
     #endif
@@ -68,12 +80,39 @@ int task_switch (task_t *task) {
 
 void task_exit (int exit_code) {
 
-    task_switch (&task_main);
-
+    if (task_atual == &task_dispatcher) 
+        task_switch(&task_main);
+    else
+        user_tasks--;
+        queue_remove((queue_t**) &ready_task_queue, (queue_t*) task_atual);
+        task_switch(&task_dispatcher);
 }
 
 int task_id () {
 
     return task_atual->id;
 
+}
+
+void task_yield() {
+    task_switch(&task_dispatcher);
+}
+
+void dispatcher() {
+    task_t *proxima;
+    while (user_tasks > 0) {
+        proxima = scheduler()
+        if (proxima != NULL) {
+            task_switch(proxima);
+        }
+    }
+    task_exit(0);
+}
+
+task_t *scheduler() {
+    if (ready_task_queue == NULL)
+        return NULL
+    task_t *first_task=ready_task_queue;
+    ready_task_queue = ready_task_queue->next;
+    return first_task;
 }
